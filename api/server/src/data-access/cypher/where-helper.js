b4a3e1ad9595e42bdcc69d76d7d922dee754cace
Esteban Canepa <esteban.canepa@rga.com>
2016-08-18 16:03:09 -0300

    api - fix where helper  operator b

--- api/server/src/data-access/cypher/where-helper.js
+++ api/server/src/data-access/cypher/where-helper.js
@@ -19,6 +19,7 @@ function queryIncludeToWhere(include, params){
 }
 
 function queryMapToWhere(model, map, alias, params){
+    console.log("map", map)
     let terms = [];
     for(let k in map){
         let term = queryKeyValueToCypher(model, k, map[k], alias, params);
@@ -66,7 +67,7 @@ function orOperatorToCypher(key, value, alias, params){
         throw new errors.GenericError("$or operator expects an array");
     let terms = [];
     value.forEach(map => {
-        let term = queryMapToWhere(map, alias, params);
+        let term = queryMapToWhere(null, map, alias, params);
         terms.push(term);
     })
     return '(' + terms.join(' OR ') + ') ';
@@ -77,7 +78,7 @@ function andOperatorToCypher(key, value, alias, params){
         throw new errors.GenericError("$and operator expects an array");
     let terms = [];
     value.forEach(map => {
-        let term = queryMapToWhere(map, alias, params);
+        let term = queryMapToWhere(null, map, alias, params);
         terms.push(term);
     })
     return '(' + terms.join(' AND ') + ') ';
@@ -100,8 +101,7 @@ function inOperatorToCypher(key, value, alias, params){
 function notOperatorToCypher(key, value, alias, params){
     if(!_.isObject(value))
         throw new errors.GenericError("$not operator expects a object");
-    let map = value.$not;
-    return 'NOT(' + queryMapToWhere(map, alias, params) + ')';
+    return 'NOT(' + queryMapToWhere(null, value, alias, params) + ')';
 }
 
 function comparisonOperatorToCypher(operator, key, value, alias, params){
@@ -177,7 +177,7 @@ function ilikeOperatorToCypher(key, value, alias) {
 }
 
 function getOperator(key, value){
-    if(typeof k === 'string' && k[0] == '$')
+    if(typeof key === 'string' && key[0] == '$')
         return key;
     
     if(_.isObject(value) && Object.keys(value).length == 1 && Object.keys(value)[0][0] == '$' )
