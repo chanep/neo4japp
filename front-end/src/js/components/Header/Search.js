3c87b5218cfc20c3ae4a5bc2100494ebbf3b9a07
Federico Tohme <federico.tohme@rga.com>
2016-08-19 13:35:56 -0300

    Make search results expand when clicking on the search icon, SKLLSRCH-21

--- front-end/src/js/components/Header/Search.js
+++ front-end/src/js/components/Header/Search.js
@@ -5,17 +5,30 @@ import Results from "./Search/Results";
 import { Link } from "react-router";
 
 export default class Search extends React.Component {
+    constructor () {
+      super();
+      this.state = {
+        results: false
+      };
+    }
+
+    showResults () {
+      this.setState({ results: true });
+    }
+
     render () {
         return (
-            <div className="search-wrapper">
-              <div className="search-wrapper__container">
-                <input type="text"/>
-                <a href="#" className="search-button-wrapper">
-                  <span className="icon-close"><span className="path1"></span><span className="path2"></span></span>
-                  <span className="icon-search"></span>
-                </a>
+            <div className="search">
+              <div className="search__input__wrapper">
+                <div className="search__input">
+                  <input type="text"/>
+                  <a href="#" className="search-button-wrapper">
+                    <span className="icon-close"><span className="path1"></span><span className="path2"></span></span>
+                    <span className="icon-search" onClick={this.showResults.bind(this)}></span>
+                  </a>
+                </div>
               </div>
-              { this.props.results && <Results /> }
+              { <Results results={this.state.results} /> }
             </div>
         );
     }
--- front-end/src/js/components/Header/Search/Results.js
+++ front-end/src/js/components/Header/Search/Results.js
@@ -5,33 +5,36 @@ import { Link } from "react-router";
 export default class Results extends React.Component {
     render () {
         return (
-          <div className="search-wrapper__results">
-            <ul>
-              <li className="category-list">Skills</li>
-              <li className="subcategory-list">
-                <ul>
-                  <li className="selected">Node</li>
-                  <li>NodeJS</li>
-                </ul>
-              </li>
-            </ul>
-            <ul>
-              <li className="category-list">Tools</li>
-              <li className="subcategory-list">
-                <ul>
-                  <li>Animator</li>
-                </ul>
-              </li>
-            </ul>
-            <ul>
-              <li className="category-list">People</li>
-              <li className="subcategory-list">
-                <ul>
-                  <li>Andrés Juarez</li>
-                </ul>
-              </li>
-            </ul>
-          </div>
+          <div className="search__results__wrapper">
+          {this.props.results ?
+            (<div className="search__results">
+              <ul>
+                <li className="category-list">Skills</li>
+                <li className="subcategory-list">
+                  <ul>
+                    <li className="selected">Node</li>
+                    <li>NodeJS</li>
+                  </ul>
+                </li>
+              </ul>
+              <ul>
+                <li className="category-list">Tools</li>
+                <li className="subcategory-list">
+                  <ul>
+                    <li>Animator</li>
+                  </ul>
+                </li>
+              </ul>
+              <ul>
+                <li className="category-list">People</li>
+                <li className="subcategory-list">
+                  <ul>
+                    <li>Andrés Juarez</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>) : null}
+            </div>
         );
     }
 }
