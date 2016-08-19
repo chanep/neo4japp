1c0abbf54ac021350c64b45334039abeba67760d
Federico Tohme <federico.tohme@rga.com>
2016-08-19 15:15:06 -0300

    Make search results collapse when clicking on the close icon, SKLLSRCH-21

--- front-end/src/js/components/Header/Search.js
+++ front-end/src/js/components/Header/Search.js
@@ -12,6 +12,10 @@ export default class Search extends React.Component {
       };
     }
 
+    hideResults() {
+      this.setState({ results: false });
+    }
+
     showResults () {
       this.setState({ results: true });
     }
@@ -22,10 +26,10 @@ export default class Search extends React.Component {
               <div className="search__input__wrapper">
                 <div className="search__input">
                   <input type="text"/>
-                  <a href="#" className="search-button-wrapper">
-                    <span className="icon-close"><span className="path1"></span><span className="path2"></span></span>
+                  <span className="search-button-wrapper">
+                    <span className="icon-close"><span className="path1"></span><span className="path2" onClick={this.hideResults.bind(this)}></span></span>
                     <span className="icon-search" onClick={this.showResults.bind(this)}></span>
-                  </a>
+                  </span>
                 </div>
               </div>
               { <Results results={this.state.results} /> }
