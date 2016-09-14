f6d544d267d523c8aad13812ef88043d1551795d
Lourdes Montano <lourdes.montano@rga.com>
2016-09-14 11:46:46 -0300

    ;ayout fix

--- front-end/src/js/components/SearchAllSkillsTable.js
+++ front-end/src/js/components/SearchAllSkillsTable.js
@@ -34,10 +34,10 @@ export default class SearchAllSkillsTable extends React.Component {
                     <div className="col -col-3">
                         <span className="table-header">Filter</span>
                     </div>
-                    <div className="col -col-3">
+                    <div className="col -col-4">
                         <span className="table-header">Category</span> 
                     </div>
-                    <div className="col -col-2">
+                    <div className="col -col-3">
                         <span className="table-header">Group</span>
                     </div>
                     <div className="col -col-2">
