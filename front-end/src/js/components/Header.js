ead8628ab3b982603287cad5d72b6394551865f6
Juan Jose Cumba <juan.cumba@rga.com>
2016-09-15 11:29:09 -0300

    Picture in header

--- front-end/src/js/components/Header.js
+++ front-end/src/js/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Search from "./Header/Search";
-
+import cookie from 'react-cookie';
 import { Link } from "react-router";
 
 export default class Header extends React.Component {
   constructor (search, loggedIn) {
     super();
-    this.state = {'search': search, 'loggedIn': loggedIn};
+    this.state = {'search': search, 'loggedIn': loggedIn, userLogged: cookie.load('currentUser')};
+    console.log("user", this.state.userLogged.image);
   }
 
   render () {
@@ -15,7 +16,15 @@ export default class Header extends React.Component {
         <header>
           <div className="header-wrapper">
             <h1><img src="img/rga-logo.png"></img></h1>
-            { this.props.loggedIn && <Link to="allocations" className="allocations-btn"><span className="ss-icon-view"></span>View Allocations <img src="img/profile-pic.png"></img></Link> }
+            {this.props.loggedIn &&
+              <Link to="allocations" className="allocations-btn">
+                <span className="ss-icon-view"></span>View Allocations
+                {(this.state.userLogged.image ?
+                  <img src={this.state.userLogged.image}></img>
+                  :"No hay imagen"
+                )}
+              </Link>
+            }
           </div>
           { this.props.search && <Search /> }
         </header>
