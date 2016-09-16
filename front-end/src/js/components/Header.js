4ae44c1dfa0ba53028dc48910c05ab87cae22120
Lourdes Montano <lourdes.montano@rga.com>
2016-09-16 16:13:47 -0300

    mnu header

--- front-end/src/js/components/Header.js
+++ front-end/src/js/components/Header.js
@@ -16,15 +16,30 @@ export default class Header extends React.Component {
         <header>
           <div className="header-wrapper">
             <h1><img src="img/rga-logo.png"></img></h1>
-            {this.props.loggedIn &&
-              <Link to="allocations" className="allocations-btn">
-                <span className="ss-icon-view"></span>View Allocations
-                {(this.state.userLogged && this.state.userLogged.image ?
-                  <img src={this.state.userLogged.image}></img>
-                  :"No hay imagen"
-                )}
-              </Link>
-            }
+            <div className="header-menu-wrapper">
+              {this.props.loggedIn &&
+                <Link to="allocations" className="allocations-btn">
+                  {/*<span className="ss-icon-view"></span>View Allocations*/}
+                  {(this.state.userLogged && this.state.userLogged.image ?
+                    <img src={this.state.userLogged.image}></img>
+                    :"No hay imagen"
+                  )}
+
+                  <ul className="header-menu">
+                    <li className="header-menu__item header-menu__item--title">Jane Smith</li>
+                    <li className="header-menu__item"><a href="#">Your Skills</a></li>
+                    <li className="header-menu__item"><a href="#">Your Work</a></li>
+                    <li className="header-menu__item"><a href="#">Your Allocations</a></li>
+                    <li className="header-menu__item"><a href="#">Manage Skills</a></li>
+                    <li className="header-menu__item"><a href="#">FAQ</a></li>
+                    <li className="header-menu__item"><a href="#">Logout</a></li>
+                  </ul> 
+
+                </Link>
+              }
+
+              
+            </div>
           </div>
           { this.props.search && <Search /> }
         </header>
