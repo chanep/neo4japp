c261f602d732c0d0c30257168d25b07f39204eaa
Federico Tohme <federico.tohme@rga.com>
2016-09-29 17:55:33 -0300

    Let qs prepare query string

--- front-end/src/js/services/SearchServices.js
+++ front-end/src/js/services/SearchServices.js
@@ -2,7 +2,7 @@ import ServicesBase from './ServicesBase';
 
 export default class SearchServices extends ServicesBase {
 	GetSearchAll(query,limit) {
-		return super.callGetServices('resource-manager/search-all', 'term='+ query + '&limit=' + limit);
+		return super.callGetServices('resource-manager/search-all', {term: query, limit: limit});
 	}
 
 	GetSearchBySkills(skillsIds,limit) {
