3263bfcbaec5f6f1b3aa7fd63b5c1358f984ce3b
Federico Tohme <federico.tohme@rga.com>
2016-10-12 12:59:10 -0300

    Fix search issue
    String queried was always one character behind the search field text.

--- front-end/src/js/components/Header/Search.js
+++ front-end/src/js/components/Header/Search.js
@@ -33,7 +33,7 @@ class Search extends React.Component {
 
       if (e.target.value.length > 0) {
         this.setState({query: e.target.value});
-        this.query();
+        this.query(e.target.value);
          this.setState({ word: e.target.value });
         this.setState({ selection: 1 });
       
@@ -75,10 +75,10 @@ class Search extends React.Component {
       this.setState({ chosenItems: currentChosenItems });
     }
 
-    query() {
+    query(queryString) {
 
       let searchService = new SearchServices();
-      searchService.GetSearchAll(this.state.query, 5).then(data =>{
+      searchService.GetSearchAll(queryString, 5).then(data =>{
          //console.log('Search new service')
          this.setState({ results: data });
          //console.log(data);
@@ -299,7 +299,7 @@ class Search extends React.Component {
                     {pills.map((pillName, index)=>{
                       return (<Pill name={pillName} removeSkill={this.removeSkill} index={index} />)
                     })}
-                    <input type="text" name="query" id="querySearch" onChange={this.updateQuery} onKeyDown={this.move} placeholder="enter search..."/>
+                    <input type="text" name="query" id="querySearch" onKeyUp={this.updateQuery} onKeyDown={this.move} placeholder="enter search..."/>
                   </div> 
                   <span className="search-button-wrapper">
                     <span className="ss-icon-close"><span className="path1"></span><span className="path2" onClick={this.clearSearch.bind(this)}></span></span>
