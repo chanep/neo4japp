7727fbb22cfd172ef599a35cf283bd2b04a02c58
Federico Tohme <federico.tohme@rga.com>
2016-10-19 13:16:28 -0300

    Add mail icon

--- front-end/src/js/components/Footer/ContactUs.js
+++ front-end/src/js/components/Footer/ContactUs.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class ContactUs extends React.Component {
     render () {
     	return(
-			<a href="#"><span className="icon-envelope">Contact Us</span></a>
+			<a href="#"><span className="ss-icon-envelope"></span> <span className="icon-envelope">Contact Us</span></a>
     	)
     }
 }
