9b5268a2b4d30f510a6b89283c48f2b550c7f4ef
Federico Tohme <federico.tohme@rga.com>
2016-11-29 17:53:50 -0300

    Implement suggest skill modal, SKLLSRCH-120

--- front-end/src/js/client.js
+++ front-end/src/js/client.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Modal from "react-modal";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Layout from "./components/Layout";
--- front-end/src/js/components/EmployeeProfile/AddSkillsList.js
+++ front-end/src/js/components/EmployeeProfile/AddSkillsList.js
@@ -5,16 +5,24 @@
 // Dependencies
 import React from 'react';
 import { Link } from 'react-router';
+import Modal from 'react-modal';
 import AddSkillItem from './AddSkillItem';
+import SkillsServices from '../../services/SkillsServices';
 
 export default class AddSkillsList extends React.Component {
     constructor(props) {
         super(props);
 
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.submitSuggestion = this.submitSuggestion.bind(this);
+
         this.state = {
             selectedGroup: 0,
         	data: []
         };
+
+        this.skillsServices = new SkillsServices();
     }
 
     componentDidMount() {
@@ -31,22 +39,63 @@ export default class AddSkillsList extends React.Component {
         });
 	}
 
+    openModal () { this.setState({open: true}); }
+
+    closeModal () { this.setState({open: false}); }
+
+    submitSuggestion () {
+        var name = document.getElementById('suggestion-name').value.trim(),
+            description = document.getElementById('suggestion-description').value.trim();
+
+        this.skillsServices.SuggestSkill(name, description).then(data => {
+            this.setState({open: false});
+        }).catch(data => {
+          
+            console.log("skills error", data);
+          
+        });
+    }
+
     render() {
         let self = this;
 
         return (
-            <div className="addSkillsList col -col-9 -col-no-gutter">
-            {this.state.data.length > 0 && this.state.selectedGroup > 0 ?
-                this.state.data.map(function(obj, key) {
-                    if (obj.id === self.state.selectedGroup) {
-                        return obj.children.map((subgroup, subgroupKey) => 
-                            <AddSkillItem data={subgroup} parent={obj} key={subgroupKey} />
-                        )
+            <div>
+                <div className="addSkillsList col -col-9 -col-no-gutter">
+                    {this.state.data.length > 0 && this.state.selectedGroup > 0 ?
+                        this.state.data.map(function(obj, key) {
+                            if (obj.id === self.state.selectedGroup) {
+                                return obj.children.map((subgroup, subgroupKey) => 
+                                    <AddSkillItem data={subgroup} parent={obj} key={subgroupKey} />
+                                )
+                            }
+                        })
+                    :
+                        <div className="selectCategory">Please, select a category</div>
                     }
-                })
-            :
-                <div className="selectCategory">Please, select a category</div>
-            }
+                    <div onClick={this.openModal} className="suggest"><span>Suggest skill/tool</span></div>
+                </div>
+                <Modal
+                    isOpen={this.state.open}
+                    onRequestClose={this.closeModal}
+                    className="ModalClass"
+                    overlayClassName="OverlayClass">
+                    <div className="modal-header">
+                        <span className="modal-close ss-icon-close" onClick={this.closeModal}><span className="path1"></span><span className="path2"></span></span>
+                    </div>
+                    <div className="modal-contents">
+                        <h2>Suggest skill/tool</h2>
+                        <div>
+                            Skill/tool name: <br/>
+                            <input id="suggestion-name" />
+                        </div>
+                        <div>
+                            Description: <br/>
+                            <textarea id="suggestion-description" />
+                        </div>
+                        <button onClick={this.submitSuggestion} value="Submit">Submit</button>
+                    </div>
+                </Modal>
             </div>
         );
     }
