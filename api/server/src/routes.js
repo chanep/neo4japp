2aceeaf488db419d2d322edce918bbbe22e46fa4
nicolasd <nicolas.debin@rga.com>
2017-04-07 17:49:00 -0300

    skills by user endpoint

--- api/server/src/routes.js
+++ api/server/src/routes.js
@@ -62,6 +62,7 @@ router.get('/approver/search-all', searchAllController.searchAll.bind(searchAllC
 router.use('/resource-manager', security.checkRole([roles.resourceManager, roles.searcher]).bind(security));
 router.get('/resource-manager/search-all', searchAllController.searchAll.bind(searchAllController));
 router.get('/resource-manager/users-by-skill', resourceManagerController.findUsersBySkill.bind(resourceManagerController));
+router.get('/resource-manager/skill-by-user', resourceManagerController.findSkillByUser.bind(resourceManagerController));
 router.get('/resource-manager/top-skill-searches', resourceManagerController.topSkillSearches.bind(resourceManagerController));
 router.get('/resource-manager/skilled-users-by-office/:skillId', security.checkRole(roles.resourceManager), resourceManagerController.skilledUsersByOffice.bind(resourceManagerController));
 router.put('/resource-manager/approval-request/:employeeId', resourceManagerController.requestApproval.bind(resourceManagerController));
