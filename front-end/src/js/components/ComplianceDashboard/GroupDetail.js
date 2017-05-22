5b6be18e788587448b1f3be927e92961ff8079b9
Eric Estes <Eric.Estes@rga.com>
2017-05-22 18:10:45 -0400

    user details

--- front-end/src/js/components/ComplianceDashboard/GroupDetail.js
+++ front-end/src/js/components/ComplianceDashboard/GroupDetail.js
@@ -1,23 +1,64 @@
 import React from "react";
 import { Link } from "react-router";
 
+import UserServices from '../../services/UserServices';
+
 export default class GroupDetail extends React.Component {
   constructor(props) {
       super(props);
 
-      this.state = {
-          users: (props.showActive ? props.data.activeUsers : props.data.inactiveUsers)
-      };
+      this.userData = new UserServices();
+      this.setInitialState(props);
   }
 
   componentWillReceiveProps(props) {
+    this.setInitialState(props);
+  }
+
+  setInitialState(props) {
+    let users = props.showActive ? props.data.activeUsers : props.data.inactiveUsers;
+
     this.state = {
-        users: (props.showActive ? props.data.activeUsers : props.data.inactiveUsers)
+        users: users.map(user => Object.assign({}, user, {open: false, title: '', manager: '', detailsLoadStatus: 'not-loaded'}))
     };
   }
 
   toggleUserPanel(userId) {
-    console.log(userId);
+    this.setState((prevState) => {
+      let users = prevState.users,
+          userIndex = users.findIndex(user => user.id === userId);
+
+      users[userIndex].open = !users[userIndex].open;
+
+      console.log(userId);
+
+      this.getUserDetails(userId);
+
+      return Object.assign({}, prevState, {users});
+    });
+  }
+
+  getUserDetails(userId) {
+    let user = this.state.users.find(user => user.id === userId);
+
+    if(user.detailsLoadStatus == 'loaded' || user.detailsLoadStatus == 'loading') {
+      return;
+    } else {
+      user.detailsLoadStatus = 'loading';
+
+      this.userData.GetUserData(userId).then(data => {
+        this.setState((prevState) => {
+          let users = prevState.users,
+              userIndex = users.findIndex(user => user.id === userId);
+
+          users[userIndex].title = data.position.name;
+          users[userIndex].manager = data.resourceManagers[0].fullname;
+          users[userIndex].detailsLoadStatus = 'loaded';
+
+          return Object.assign({}, prevState, {users});
+        });
+      });
+    }
   }
 
   render () {
@@ -33,9 +74,17 @@ export default class GroupDetail extends React.Component {
           </thead>
           <tbody>
             {this.state.users.map(user =>
-              <tr key={user.id}>
+              <tr key={user.id} className={user.open ? 'open' : 'closed' }>
                 <td className="compliance-dashboard-table__checkbox">check</td>
-                <td className="compliance-dashboard-table__name">{user.fullname}</td>
+                <td className="compliance-dashboard-table__name">
+                  <div>
+                    {user.fullname}
+                    <div className="compliance-dashboard-table__name-details">
+                      {user.title}<br />
+                      Manager: <strong>{user.manager}</strong>
+                    </div>
+                  </div>
+                </td>
                 <td className="compliance-dashboard-table__action"><button onClick={() => this.toggleUserPanel(user.id)}><span className="ss-icon-left-arrow"></span></button></td>
               </tr>
             )}
