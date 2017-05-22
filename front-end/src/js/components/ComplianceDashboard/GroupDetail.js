eefe20185128de9d2fd6709adf33f38175899929
Eric Estes <Eric.Estes@rga.com>
2017-05-22 15:56:46 -0400

    dashboard table styling

--- front-end/src/js/components/ComplianceDashboard/GroupDetail.js
+++ front-end/src/js/components/ComplianceDashboard/GroupDetail.js
@@ -2,10 +2,45 @@ import React from "react";
 import { Link } from "react-router";
 
 export default class GroupDetail extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          users: (props.showActive ? props.data.activeUsers : props.data.inactiveUsers)
+      };
+  }
+
+  componentWillReceiveProps(props) {
+    this.state = {
+        users: (props.showActive ? props.data.activeUsers : props.data.inactiveUsers)
+    };
+  }
+
+  toggleUserPanel(userId) {
+    console.log(userId);
+  }
+
   render () {
     return (
       <div>
-        {this.props.showActive ? 'active' : 'inactive' } group detail: ({this.props.showActive ? this.props.data.activeUsers.length : this.props.data.inactiveUsers.length })
+        {this.props.showActive ? 'active' : 'inactive' } group detail: ({this.state.users.length})
+
+        <table className="compliance-dashboard-table">
+          <thead>
+            <tr>
+              <th colSpan="3">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map(user =>
+              <tr key={user.id}>
+                <td className="compliance-dashboard-table__checkbox">check</td>
+                <td className="compliance-dashboard-table__name">{user.fullname}</td>
+                <td className="compliance-dashboard-table__action"><button onClick={() => this.toggleUserPanel(user.id)}><span className="ss-icon-left-arrow"></span></button></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     );
   }
--- front-end/src/js/components/ComplianceDashboard/GroupOverview.js
+++ front-end/src/js/components/ComplianceDashboard/GroupOverview.js
@@ -8,21 +8,21 @@ export default class GroupOverview extends React.Component {
         <div className="compliance-dashboard-item__header">Searchable Employees</div>
         <div className="compliance-dashboard-item__detail-list">
           <Link to={this.props.location.pathname + '/active'} className="compliance-dashboard-item__detail">
-          <div className="compliance-dashboard-item__detail-title">Active Users</div>
-          <div className="compliance-dashboard-item__detail-value">{this.props.data.activeUsers.length}</div>
-          <div className="compliance-dashboard-item__detail-cta">more</div>
-        </Link>
-        <Link to={this.props.location.pathname + '/inactive'} className="compliance-dashboard-item__detail">
-        <div className="compliance-dashboard-item__detail-title">Inactive Users</div>
-        <div className="compliance-dashboard-item__detail-value">{this.props.data.inactiveUsers.length}</div>
-        <div className="compliance-dashboard-item__detail-cta">more</div>
-      </Link>
-      <div className="compliance-dashboard-item__detail compliance-dashboard-item__detail--wide">
-        <div className="compliance-dashboard-item__detail-title">Compliance</div>
-        <div className="compliance-dashboard-item__detail-value">{Math.round(this.props.data.activeUsers.length / (this.props.data.activeUsers.length + this.props.data.inactiveUsers.length) * 100)}%</div>
+            <div className="compliance-dashboard-item__detail-title">Active Users</div>
+            <div className="compliance-dashboard-item__detail-value">{this.props.data.activeUsers.length}</div>
+            <div className="compliance-dashboard-item__detail-cta">more</div>
+          </Link>
+          <Link to={this.props.location.pathname + '/inactive'} className="compliance-dashboard-item__detail">
+            <div className="compliance-dashboard-item__detail-title">Inactive Users</div>
+            <div className="compliance-dashboard-item__detail-value">{this.props.data.inactiveUsers.length}</div>
+            <div className="compliance-dashboard-item__detail-cta">more</div>
+          </Link>
+          <div className="compliance-dashboard-item__detail compliance-dashboard-item__detail--wide">
+            <div className="compliance-dashboard-item__detail-title">Compliance</div>
+            <div className="compliance-dashboard-item__detail-value">{Math.round(this.props.data.activeUsers.length / (this.props.data.activeUsers.length + this.props.data.inactiveUsers.length) * 100)}%</div>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
     );
   }
 }
